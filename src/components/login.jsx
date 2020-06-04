e5070138a3df0339039b849443c2dcc43d07c7f0
reddynr <narasimaha.reddy@tecnotree.com>
2020-06-04 08:38:46 +0530

    Login screen

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import PropTypes from 'prop-types';
-import { Grid, Card, Box, Typography } from "@material-ui/core";
+import { Grid, Paper, Box, Typography } from "@material-ui/core";
 
 const Login = (props) => {
   return (
     <Box>
       <Grid container justify="center">
-        <Grid item xs={11} lg={6} sm={10} md={8}>
-          <Card>
+        <Grid item xs={12} lg={6} sm={10} md={8}>
+          <Paper>
             <Box p={3}>
               <Typography>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -22,7 +22,7 @@ const Login = (props) => {
                 Lorem Ipsum.
               </Typography>
             </Box>
-          </Card>
+          </Paper>
         </Grid>
       </Grid>
     </Box>
