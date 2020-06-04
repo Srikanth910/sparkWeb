5d6f7773d477c49bf9932a2eae7818f99be82bbb
reddynr <narasimaha.reddy@tecnotree.com>
2020-06-04 08:27:59 +0530

    Login screen

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React from "react";
 // import PropTypes from 'prop-types';
+import { Grid, Card, Box, Typography } from "@material-ui/core";
 
-const Login = props => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+const Login = (props) => {
+  return (
+    <Box>
+      <Grid container justify="center">
+        <Grid item xs={11} lg={6} sm={10} md={8}>
+          <Card>
+            <Box p={3}>
+              <Typography>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </Typography>
+            </Box>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
-Login.propTypes = {
-    
-};
+Login.propTypes = {};
 
 export default Login;
